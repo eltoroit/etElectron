1039400e7b9d59cddbe3a7bea47a07903cf3d30d
Andres Perez <andresperezf@yahoo.com>
2022-10-20 09:52:07 -0400

    Simple interprocess communication

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,6 @@
-// All the Node.js APIs are available in the preload process. It has the same sandbox as a Chrome extension.
+// Preload scripts are injected before a web page loads in the renderer. All the Node.js APIs are available in the preload process. It has the same sandbox as a Chrome extension.
+const { contextBridge } = require("electron");
+
 window.addEventListener("DOMContentLoaded", () => {
 	const replaceText = (selector, text) => {
 		const element = document.getElementById(selector);
@@ -8,4 +10,10 @@ window.addEventListener("DOMContentLoaded", () => {
 	for (const dependency of ["chrome", "node", "electron"]) {
 		replaceText(`${dependency}-version`, process.versions[dependency]);
 	}
+});
+
+contextBridge.exposeInMainWorld("versions", {
+	node: () => process.versions.node,
+	chrome: () => process.versions.chrome,
+	electron: () => process.versions.electron
 });
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,3 +1,8 @@
+const information = document.getElementById("info");
+information.innerHTML = `This app is using Chromium (v${versions.chrome()}),<br/>
+                         Node.js (v${versions.node()}),<br/>
+                         and Electron (v${versions.electron()})`;
+
 setTimeout(() => {
 	alert("Hello");
 }, 1000);
